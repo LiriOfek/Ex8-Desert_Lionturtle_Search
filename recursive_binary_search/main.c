2a6ae670aa862fdd3909d67845ff5c4d57de5c7c
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-04-08 15:18:46 +0300

    add main file with documentation

---
+++ recursive_binary_search/main.c
@@ -0,0 +1,6 @@
+/********************************************************\
+File Name: main.c
+Author: Liri (8/4/2020)
+Purpose: This file find the index of requested number in given array
+		 by recursive binary search
+\********************************************************/
